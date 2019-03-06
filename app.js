7cdb6a04be6f79fe8e20dbb2163b3965dc0ad4d1
Manish Gupta <manneeshgupta@outlook.in>
2019-03-06 17:47:50 +0530

    Modified

--- app.js
+++ app.js
@@ -8,28 +8,37 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
   $scope.dishes = "";
+  $scope.note = "Note: An empty item with ',  ,' is NOT considered in the count.";
+  $scope.dishColor = {};
+  $scope.dishColor.colorStyle = "";
 
   $scope.dishCountCheck = function () {
     var dishCnt = 0;
     var dishNames = $scope.dishes.split(",");
     for(var cnt=0; cnt <= dishNames.length; cnt++)
     {
-      if(dishNames[cnt] != null && dishNames[cnt] != '')
+      if(dishNames[cnt] != null)
       {
-        dishCnt++;
+        if(dishNames[cnt].trim() != '')
+        {
+          dishCnt++;
+        }
       }
     }
     if(dishCnt == 0)
     {
       $scope.dishCountMessage = "Please enter data first";
+      $scope.dishColor.colorStyle = "red";
     }
     else if(dishCnt > 0 && dishCnt <= 3)
     {
       $scope.dishCountMessage = "Enjoy!";
+      $scope.dishColor.colorStyle = "green";
     }
     else if(dishCnt > 3)
     {
       $scope.dishCountMessage = "Too much!";
+      $scope.dishColor.colorStyle = "green";
     }
   };
 }
